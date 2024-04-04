b5c8890e8f71cf093727f3c8953399c2e2e11d8b
JendaSt <jendastraka@seznam.cz>
2024-04-04 13:43:13 +0200

    Add project files.

---
+++ PageObject/WikiPagecs.cs
@@ -0,0 +1,21 @@
+﻿namespace Selenium.PageObject
+{
+    using OpenQA.Selenium;
+
+    public class WikiPage
+    {
+        public IWebDriver driver;
+
+        public WikiPage(IWebDriver _driver)
+        {
+            driver = _driver;
+        }
+
+        public IWebElement searchField => driver.FindElement(By.Name("search"));
+        public IWebElement searchBtn => driver.FindElement(By.XPath(".//*[@class='cdx-button cdx-button--action-default cdx-button--weight-normal cdx-button--size-medium cdx-button--framed cdx-search-input__end-button']"));
+        public IWebElement bodyContent => driver.FindElement(By.Id("bodyContent")).FindElement(By.Id("mw-content-text")).FindElement(By.TagName("p"));
+        public IWebElement firstHeading => driver.FindElement(By.Id("firstHeading"));
+        public IWebElement pageTitle => firstHeading.FindElement(By.ClassName("mw-page-title-main"));
+        public IWebElement wikiLink => bodyContent.FindElement(By.CssSelector("[href ^= '/wiki/']"));
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,63 @@
+namespace Selenium
+{
+    using System;
+    using System.Threading;
+    using NUnit.Framework;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using Selenium.PageObject;
+
+    public class Test
+    {
+        private IWebDriver driver = new ChromeDriver();
+        private WikiPage? _WikiPage;
+        private string page = "Filozofie";
+
+        [Test]
+        public void PhilosophyTest()
+        {
+            OpenUrl("https://cs.wikipedia.org/wiki");
+            OpenArticle(page);
+            RedirectPage();
+            driver.Close();
+        }
+
+        public void OpenUrl(string url)
+        {            
+            driver.Manage().Window.Maximize();
+            driver.Url = url;            
+        }
+
+        public void OpenArticle(string article)
+        {
+            _WikiPage = new WikiPage(driver);
+            _WikiPage.searchField.SendKeys(article);
+            Thread.Sleep(50);
+            _WikiPage.searchBtn.Click();
+        }
+
+        public void RedirectPage()
+        {
+            _WikiPage = new WikiPage(driver);            
+            int i = 0; 
+            do
+            {                
+                GetLink().Click();
+                Console.WriteLine(_WikiPage.pageTitle.Text);
+                i++;
+            }
+            while (_WikiPage.pageTitle.Text != page);
+            
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Number of redirect = " + i);
+        }
+
+        public IWebElement GetLink()
+        {
+            _WikiPage = new WikiPage(driver);
+            IWebElement link = _WikiPage.wikiLink;
+            Console.WriteLine("-> " + link.Text);
+            return link;
+        }
+    }
+}
