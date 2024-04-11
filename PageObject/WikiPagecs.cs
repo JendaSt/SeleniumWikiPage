c51f5b3114f038eb46239ec145dff07688ea1162
JendaSt <jendastraka@seznam.cz>
2024-04-11 12:13:46 +0200

    Add Cycle detected

--- PageObject/WikiPagecs.cs
+++ PageObject/WikiPagecs.cs
@@ -13,15 +13,32 @@
 
         public IWebElement searchField => driver.FindElement(By.Name("search"));
         public IWebElement searchBtn => driver.FindElement(By.XPath(".//*[@class='cdx-button cdx-button--action-default cdx-button--weight-normal cdx-button--size-medium cdx-button--framed cdx-search-input__end-button']"));
-        public IWebElement bodyContent => driver.FindElement(By.Id("bodyContent")).FindElement(By.Id("mw-content-text")).FindElement(By.TagName("p"));
+        public IWebElement bodyContent => driver.FindElement(By.Id("bodyContent")).FindElement(By.Id("mw-content-text")).FindElement(By.CssSelector(".mw-content-ltr.mw-parser-output")).FindElement(By.TagName("p"));
+        public IWebElement wikiLink => bodyContent.FindElement(By.CssSelector("[href ^= '/wiki/']"));
         public IWebElement firstHeading => driver.FindElement(By.Id("firstHeading"));
         public IWebElement pageTitle => firstHeading.FindElement(By.ClassName("mw-page-title-main"));
-        public IWebElement wikiLink => bodyContent.FindElement(By.CssSelector("[href ^= '/wiki/']"));
-
         public IWebElement GetLink()
-        {           
-            Console.WriteLine("-> " + wikiLink.Text);
-            return wikiLink;
+        {
+            try
+            {
+                Console.WriteLine("-> p: " + wikiLink.Text);
+                return wikiLink;
+            }
+            catch (NoSuchElementException)
+            {
+                try
+                {
+                    IWebElement wikiLink = driver.FindElement(By.Id("bodyContent")).FindElement(By.Id("mw-content-text")).FindElement(By.CssSelector(".mw-content-ltr.mw-parser-output")).FindElement(By.TagName("ul")).FindElement(By.CssSelector("[href ^= '/wiki/']"));
+                    Console.WriteLine("-> ul: " + wikiLink.Text);
+                    return wikiLink;
+                }
+                catch (NoSuchElementException)
+                {
+                    IWebElement wikiLink = driver.FindElement(By.Id("bodyContent")).FindElement(By.Id("mw-content-text")).FindElement(By.CssSelector(".mw-content-ltr.mw-parser-output")).FindElement(By.CssSelector("[href ^= '/wiki/']"));
+                    Console.WriteLine("-> " + wikiLink.Text);
+                    return wikiLink;
+                }             
+            }       
         }
     }
 }
--- Test.cs
+++ Test.cs
@@ -1,4 +1,4 @@
-namespace Selenium
+﻿namespace Selenium
 {
     using System;
     using System.Threading;
@@ -11,13 +11,13 @@ namespace Selenium
     {
         private IWebDriver driver = new ChromeDriver();
         private WikiPage _WikiPage;
-        private string page = "Filozofie";
+        private string targetPage = "Filozofie";
 
         [Test]
         public void PhilosophyTest()
         {
             OpenUrl("https://cs.wikipedia.org/wiki");
-            OpenArticle(page);
+            OpenArticle("Filozofie");
             RedirectPage();
             driver.Close();
         }
@@ -32,25 +32,33 @@ namespace Selenium
         {
             _WikiPage = new WikiPage(driver);
             _WikiPage.searchField.SendKeys(article);
-            Thread.Sleep(100);
+            Thread.Sleep(300);
             _WikiPage.searchBtn.Click();
         }
 
         public void RedirectPage()
-        {           
-            int i = 0; 
+        {         
+            int i = 0;
+            List<string> pageHistory = new List<string>();
             do
             {
-                _WikiPage.GetLink().Click();
-                Console.WriteLine(_WikiPage.pageTitle.Text);
-                i++;
+                _WikiPage.GetLink().Click();                                               
+                if (i > 1 && pageHistory.Contains(_WikiPage.pageTitle.Text))                    
+                {
+                    Console.WriteLine("-------------");
+                    Console.WriteLine("Cycle detected");
+                    break;
+                } else
+                {
+                    Console.WriteLine(_WikiPage.pageTitle.Text);
+                    pageHistory.Add(_WikiPage.pageTitle.Text);
+                    i++;
+                }                
             }
-            while (_WikiPage.pageTitle.Text != page);
+            while (_WikiPage.pageTitle.Text != targetPage);
             
             Console.WriteLine("----------------------");
             Console.WriteLine("Number of redirect = " + i);
         }
-
-        
-    }
+     }
 }
